980c594a55c824240ae226c978c403cc88a361c2
Georg Guba <georgguba@gmail.com>
2015-11-25 14:20:13 +0100

    notes on how to build QEMU with VDE support

--- start_network.sh
+++ start_network.sh
@@ -1,9 +1,9 @@
 #bin/bash
 
-#vde_switch -d -s /tmp/switch1
+vde_switch -d -s /tmp/switch1
 
 sudo vde_tunctl -u opossum
 sudo ifconfig tap0 192.168.0.254 up
 sudo route add -host 192.168.0.10 dev tap0
 
-#vde_plug2tap --daemon -s /tmp/switch1 tap0
+vde_plug2tap --daemon -s /tmp/switch1 tap0
